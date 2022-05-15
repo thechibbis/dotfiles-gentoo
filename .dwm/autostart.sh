30be16f4b9285ab89c4774d6b7fd018ac72ea535
thechibbis <guilhermedeoliveira.menezes@gmail.com>
2022-05-15 02:27:58 -0300

    changed terminal

---
+++ .dwm/autostart.sh
@@ -0,0 +1,15 @@
+#!/bin/sh 
+
+## wallpaper & picom
+feh --bg-fill /home/thechibbis/jdm-gtr.jpg &
+picom --config /home/thechibbis/.config/picom/picom.conf &
+
+## Some programs
+discord &
+steam &
+flameshot &
+
+## dwm status bar
+dwmblocks &
+
+
